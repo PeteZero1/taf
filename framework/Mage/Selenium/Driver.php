c43fc3fecf3d072f632f9f38c3fb271cabaf7080
PeteZero1 <peter.boulderstone@zero1.co.uk>
2015-07-21 14:29:20 +0100

    Debugging an issue with FireFox crashing pre-test.

--- framework/Mage/Selenium/Driver.php
+++ framework/Mage/Selenium/Driver.php
@@ -51,6 +51,8 @@ class Mage_Selenium_Driver extends PHPUnit_Extensions_SeleniumTestCase_Driver
      */
     public function stopBrowserSession()
     {
+        return parent::stop();
+
         if (!isset($this->sessionId)) {
             return;
         }
